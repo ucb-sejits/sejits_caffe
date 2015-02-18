10e50b5a2a980cb1060b8dc00249019d5f6d6486
Leonard Truong <leonardtruong@gmail.com>
2015-02-17 16:58:40 -0800

    Migrate to inhouse convolve for test

---
+++ sejits_caffe/layers/tests/conv_test.tmpl.c
@@ -0,0 +1,62 @@
+void conv(float* in, float* weights, float* bias, float* out) {
+  // Kernel size, stride, and pad
+  int kernel_h, kernel_w;
+  kernel_h = $kernel_size;
+  kernel_w = $kernel_size;
+  int pad_h, pad_w;
+  pad_h = $pad;
+  pad_w = $pad;
+  int stride_h, stride_w;
+  stride_h = $stride;
+  stride_w = $stride;
+  // Groups
+  int groups = $group;
+  int o_g = $out_c / groups;
+  int k_g = $in_c / groups;
+  int o_head, k_head;
+  // Convolution
+  # pragma omp parallel for
+  for (int n = 0; n < $out_num; n++) {
+    for (int g = 0; g < groups; g++) {
+      o_head = o_g * g;
+      k_head = k_g * g;
+      for (int o = 0; o < o_g; o++) {
+        for (int k = 0; k < k_g; k++) {
+          for (int y = 0; y < $out_h; y++) {
+            for (int x = 0; x < $out_w; x++) {
+              for (int p = 0; p < kernel_h; p++) {
+                for (int q = 0; q < kernel_w; q++) {
+                  int in_y = y * stride_h - pad_h + p;
+                  int in_x = x * stride_w - pad_w + q;
+                  if (in_y >= 0 && in_y < $in_h
+                      && in_x >= 0 && in_x < $in_w) {
+                    int out_idx = n * $out_c * $out_h * $out_w + \
+                      (o + o_head) * $out_h * $out_w + y * $out_w + x;
+                    int in_idx = n * $in_c * $in_h * $in_w + \
+                      (k + k_head) * $in_h * $in_w + in_y * $in_w + in_x;
+                    int weight_idx = (o + o_head) * $weight_c * $weight_h * $weight_w + \
+                      k * $weight_h * $weight_w + p * $weight_w + q;
+                    out[out_idx] += in[in_idx] * weights[weight_idx];
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  // Bias
+  if ($bias_term) {
+    for (int n = 0; n < $out_num; n++) {
+      for (int o = 0; o < $out_c; o++) {
+        for (int y = 0; y < $out_h; y++) {
+          for (int x = 0; x < $out_w; x++) {
+            out[n * $out_c * $out_h * $out_w + o *
+                $out_h * $out_w + y * $out_w + x] += bias[o];
+          }
+        }
+      }
+    }
+  }
+}
--- sejits_caffe/layers/tests/test_conv_layer.py
+++ sejits_caffe/layers/tests/test_conv_layer.py
@@ -1,5 +1,4 @@
 from sejits_caffe.layers.conv_layer import ConvLayer
-from scipy.ndimage.filters import convolve
 import numpy as np
 import sejits_caffe.caffe_pb2 as caffe_pb2
 from google.protobuf import text_format
@@ -7,19 +6,24 @@ import os
 from hindemith.types.hmarray import hmarray
 import unittest
 from ctree.jit import LazySpecializedFunction, ConcreteSpecializedFunction
-from ctree.c.nodes import CFile
+from ctree.c.nodes import CFile, Constant
 from ctree.nodes import Project
 from ctree.templates.nodes import FileTemplate
+import ctypes as ct
+import ast
 
 
 class ConvConcrete(ConcreteSpecializedFunction):
     def __init__(self, entry_name, proj, entry_type):
-        pass
+        self._c_function = self._compile(entry_name, proj, entry_type)
+
+    def __call__(self, *args):
+        self._c_function(*args)
 
 
 class NaiveConv(LazySpecializedFunction):
     def __init__(self, conv_param):
-        super(NaiveConv, self).__init__(None)
+        super(NaiveConv, self).__init__(ast.Module())
         self.conv_param = conv_param
 
     def args_to_subconfig(self, args):
@@ -34,28 +38,32 @@ class NaiveConv(LazySpecializedFunction):
         pad = conv_param.pad
         stride = conv_param.stride
         group = conv_param.group
-        out_c, out_h, out_w = arg_cfg['out']._shape_
-        in_ptr_c, in_ptr_h, in_ptr_w = arg_cfg['in_ptr']._shape_
-        weights_c, weights_h, weights_w = arg_cfg['weights']._shape_
+        out_num, out_c, out_h, out_w = arg_cfg['out']._shape_
+        in_ptr_num, in_ptr_c, in_ptr_h, in_ptr_w = arg_cfg['in_ptr']._shape_
+        weights_g, weights_c, weights_h, weights_w = arg_cfg['weights']._shape_
         return [CFile('conv', [
             FileTemplate(
-                'conv_test.tmpl.c',
+                os.path.dirname(os.path.realpath(__file__)) + '/conv_test.tmpl.c',
                 {
-                    'kernel_size': kernel_size,
-                    'pad': pad,
-                    'stride': stride,
-                    'group': group,
-                    'out_c': out_c,
-                    'out_h': out_h,
-                    'out_w': out_w,
-                    'in_c': in_ptr_c,
-                    'in_h': in_ptr_h,
-                    'in_w': in_ptr_w,
-                    'weights_c': weights_c,
-                    'weights_h': weights_h,
-                    'weights_w': weights_w,
+                    'kernel_size': Constant(kernel_size),
+                    'pad': Constant(pad),
+                    'stride': Constant(stride),
+                    'group': Constant(group),
+                    'out_num': Constant(out_num),
+                    'out_c': Constant(out_c),
+                    'out_h': Constant(out_h),
+                    'out_w': Constant(out_w),
+                    'in_num': Constant(in_ptr_num),
+                    'in_c': Constant(in_ptr_c),
+                    'in_h': Constant(in_ptr_h),
+                    'in_w': Constant(in_ptr_w),
+                    'weight_g': Constant(weights_g),
+                    'weight_c': Constant(weights_c),
+                    'weight_h': Constant(weights_h),
+                    'weight_w': Constant(weights_w),
+                    'bias_term': Constant(1)
                 }
-        )])]
+        )], config_target='omp')]
 
     def finalize(self, files, program_cfg):
         arg_cfg, tune_cfg = program_cfg
@@ -63,17 +71,9 @@ class NaiveConv(LazySpecializedFunction):
         entry_type = (None, )
         for name in ['in_ptr', 'weights', 'bias', 'out']:
             entry_type += (arg_cfg[name], )
-        fn = ConvConcrete('conv', proj, )
+        fn = ConvConcrete('conv', proj, ct.CFUNCTYPE(*entry_type))
         return fn
         
-
-
-def numpy_convolve(batch, weights, expected):
-    for i in range(batch.shape[0]):
-        for n in range(25):
-            for channel in range(batch.shape[1]):
-                expected[i][n] += convolve(batch[i][channel],
-                                           weights[n][channel])[5:-5, 5:-5]
 
 
 path = os.path.dirname(os.path.realpath(__file__))
@@ -96,6 +96,7 @@ class ConvLayerTest(unittest.TestCase):
 
     def _forward_test(self, backend):
         conv = ConvLayer(param.layers[0])
+        expected_conv = NaiveConv(param.layers[0].convolution_param)
         conv.backend = backend
         actual = hmarray(np.zeros(actual_shape, np.float32))
         expected = np.zeros(expected_shape, np.float32)
@@ -105,7 +106,7 @@ class ConvLayerTest(unittest.TestCase):
         conv.forward(hmarray(in_batch), actual)
         actual = actual.reshape((5, 25, height_out, width_out))
         new_weights = conv.weights.reshape(25, 3, 11, 11)
-        numpy_convolve(in_batch, new_weights, expected)
+        expected_conv(in_batch, new_weights, conv.bias, expected)
         actual.copy_to_host_if_dirty()
         self._check(actual, expected)
 
