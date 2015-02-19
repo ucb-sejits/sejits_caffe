68d9498a266938ed8ab43b3436ee0210966c34f1
Leonard Truong <leonardtruong@gmail.com>
2015-02-18 18:44:15 -0800

    Update build script for osx

--- build_clBLAS.sh
+++ build_clBLAS.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
+git submodule update --init
 cd clBLAS
 mkdir build && cd build
 cmake ../src -DCMAKE_BUILD_TYPE=Release -DBUILD_KTEST=OFF
 CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 make -j$CORES && make install
 cd ../../
-cp clBLAS/build/package/lib64/libclBLAS.so sejits_caffe/layers
+if [ "$(uname)" == "Darwin" ]; then
+    cp clBLAS/build/package/lib64/libclBLAS.dylib sejits_caffe/layers
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    cp clBLAS/build/package/lib64/libclBLAS.so sejits_caffe/layers
+fi
 rm -rf clBLAS/build
