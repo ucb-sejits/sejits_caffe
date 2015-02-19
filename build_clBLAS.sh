525836834e6b149a26a38ce6e6ef6d1199c23bff
Leonard Truong <leonardtruong@gmail.com>
2015-02-18 17:18:44 -0800

    Remove build dir after building

--- build_clBLAS.sh
+++ build_clBLAS.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 cd clBLAS
 mkdir build && cd build
 cmake ../src -DCMAKE_BUILD_TYPE=Release -DBUILD_KTEST=OFF
@@ -5,3 +6,4 @@ CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 make -j$CORES && make install
 cd ../../
 cp clBLAS/build/package/lib64/libclBLAS.so sejits_caffe/layers
+rm -rf clBLAS/build
