22b9e7275a87a9d45e3d275cb00d296709832df0
Leonard Truong <leonardtruong@gmail.com>
2015-03-11 14:35:22 -0700

    Update conv test to use alexnet

--- tests/operations/test_relu.py
+++ tests/operations/test_relu.py
@@ -11,14 +11,14 @@ class TestRelu(unittest.TestCase):
     def test_simple(self):
         bottom = Array.rand(256, 256).astype(np.float32) * 255
         actual = Array.zeros(bottom.shape, np.float32)
-        relu(bottom, actual, 0.0)
+        relu(bottom, bottom, actual, 0.0)
         expected = np.clip(bottom, 0.0, float('inf'))
         self._check(actual, expected)
 
     def test_nonzero_slope(self):
         bottom = Array.rand(256, 256).astype(np.float32) * 255
         actual = Array.zeros(bottom.shape, np.float32)
-        relu(bottom, actual, 2.4)
+        relu(bottom, bottom, actual, 2.4)
         expected = np.clip(bottom, 0.0, float('inf')) + \
             2.4 * np.clip(bottom, float('-inf'), 0.0)
         self._check(actual, expected)
